6e50817bc20bf383a2962d6bfce15750e0bcaf92
mrw <lwilmoth@eriesd.org>
2025-04-01 10:40:13 -0400

    added setup

---
+++ setup.md
@@ -0,0 +1,8 @@
+```
+npx expo init project_name
+```
+
+Run the commands in setup_commands.ps1
+
+Install a Device on Android Studio
++More Actions> Virtual Device Manage>"+">Pixel
---
+++ setup_commands.ps1
@@ -0,0 +1,7 @@
+
+$env:PATH += ";C:\Users\lwilmoth\AppData\Roaming\npm"
+
+
+$env:ANDROID_HOME = "C:\Users\lwilmoth\AppData\Local\Android\Sdk"
+$env:PATH += ";" + $ANDROID_HOME + "\emulator"
+$env:PATH += ";" + $ANDROID_HOME + "\platform-tools"
